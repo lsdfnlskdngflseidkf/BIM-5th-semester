384a9097307a001fdb1b0665ae3ba164ee48c180
Nullproj <par@ujwalp.com.np>
2024-06-08 17:46:48 +0545

    sorting with built in functions

--- practice/list/sorting.py
+++ practice/list/sorting.py
@@ -1,6 +1,8 @@
 import random
 lis=[1,2,3,4,5,6,7,8,9]
-random.shuffle(lis)
+random.shuffle(lis)# shuffle a list (needs a specific class to be imported)
 print(lis)
-lis.sort()
+lis.sort() #sort a list (method 1)
 print(lis)
+random.shuffle(lis)# shuffle a list (needs a specific class to be imported)
+sorted_lis=sorted(lis) #method 2 of sorting a list
