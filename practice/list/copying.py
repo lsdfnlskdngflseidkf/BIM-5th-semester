c52c6d2d4e27d0e28ff268a4856f42aafeaead11
Nullproj <par@ujwalp.com.np>
2024-06-08 17:50:45 +0545

    copying list three ways

--- practice/list/copying.py
+++ practice/list/copying.py
@@ -1,2 +1,11 @@
+# import random
 lis=[1,2,3,4,5,6,7,8,9]
-copy_lis=lis.copy()
+# random.shuffle(lis)
+copy_lis=lis.copy() # using the copy function
+print(copy_lis)
+# random.shuffle(lis)
+copy=lis[:] # copy using slicing 
+print(copy)
+# random.shuffle(lis)
+cop=list(lis)
+print(cop)
