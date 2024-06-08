0b9c0f71ea8565f47777a7843fb2745c30598636
Nullproj <par@ujwalp.com.np>
2024-06-08 17:37:09 +0545

    lists insertion ,deletion and using with loops in various ways

---
+++ practice/string/list/looping_over.py
@@ -0,0 +1,8 @@
+lis=[1,2,3,4,5,6,7,8,9]
+leng=len(lis) # len function can be used as well
+print(leng)
+# for i in range(8):
+    # print(lis[i])
+# or using foreach loop as
+for index,value in enumerate(lis):
+    print(f"{index} {value}")
