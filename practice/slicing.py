3bdc2821ec79bf369ea2c63b4d5762763072adec
Nullproj <par@ujwalp.com.np>
2024-06-08 14:46:07 +0545

    slicing practice and string indexing

---
+++ practice/slicing.py
@@ -0,0 +1,4 @@
+example="This is an example string"
+print(example[0:4])
+print(example[5:7])
+print(example[9:])
---
+++ practice/string/ind.py
@@ -0,0 +1,4 @@
+string1="Hello World"
+print(string1[4]) #access the fourth characte
+print(string1[1]) # acccess the first character
+print(string1[-1]) #access the last character
