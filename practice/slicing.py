7754123d0e044c3103f173210c607e38509a131d
BMCBim <something123sdhgk@proton.me>
2024-06-09 14:59:39 +0545

    changes very tiny

--- practice/slicing.py
+++ practice/slicing.py
@@ -1,6 +1,6 @@
 example="Thisxis an example string"
-print(example[0:4])
 print(example[5:7])
+print(example[:4])
 print(example[9:])
 ex="Python is awesome!"
 print(ex[-7:-2])
---
+++ practice/string/strip.py
@@ -0,0 +1,9 @@
+example="This is an Example string"
+print(example.upper())
+print(example.lower())
+nosp=example.strip()
+print(nosp)
+another=example.replace("i","j")
+print(another)
+sep=example.split(" ")
+print(sep)
