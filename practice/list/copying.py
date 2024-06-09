6d6e5ed0eb304b8b9c53b577e1dff8b4a72b95c5
BMCBim <something123sdhgk@proton.me>
2024-06-09 14:47:18 +0545

    tiny  changes

--- practice/list/copying.py
+++ practice/list/copying.py
@@ -2,10 +2,12 @@
 lis=[1,2,3,4,5,6,7,8,9]
 # random.shuffle(lis)
 copy_lis=lis.copy() # using the copy function
-print(copy_lis)
+copy_liis=lis # assigning it without doing anything
+print(copy_liis)
+# print(copy_lis)
 # random.shuffle(lis)
 copy=lis[:] # copy using slicing 
-print(copy)
+# print(copy)
 # random.shuffle(lis)
 cop=list(lis)
-print(cop)
+# print(cop)
--- practice/slicing.py
+++ practice/slicing.py
@@ -2,3 +2,5 @@ example="Thisxis an example string"
 print(example[0:4])
 print(example[5:7])
 print(example[9:])
+ex="Python is awesome!"
+print(ex[-7:-2])
