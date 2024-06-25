d7f0842f6926ce8a032b79671d1274bea18e516c
BMCBim <something123sdhgk@proton.me>
2024-06-25 15:13:50 +0545

    factoria of a number

---
+++ python/5A/4.py
@@ -0,0 +1,11 @@
+#WAP to calculate the factorial of a number
+def factorial(var):
+    fact=1
+    if var==0 or var==1:
+        return 1
+    for i in range(1,var+1):
+        fact*=i
+    return fact
+
+inp=int(input("Enter the number to calculate the factorial of:"))
+print(factorial(inp))
