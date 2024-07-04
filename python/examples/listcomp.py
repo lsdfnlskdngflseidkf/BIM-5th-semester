ba150028a890ac280f68474a125823047d1e7beb
BMCBim <something123sdhgk@proton.me>
2024-07-04 15:12:47 +0545

    test list comprehension

--- python/examples/listcomp.py
+++ python/examples/listcomp.py
@@ -1,4 +1,5 @@
 list1=["apple","ball","car","dog","egg"]
-list2=[e for e in list1 if 'a' not in e]
+# list2=[e for e in list1 if 'a' not in e]
 # print(list2)
 list1.sort()
+[print(e) for e in list1]
