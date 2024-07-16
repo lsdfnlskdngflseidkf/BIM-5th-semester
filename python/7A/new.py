8306db9ebbb9b79bf3e53bdc9e19f27e623d769a
BMCBim <something123sdhgk@proton.me>
2024-07-16 15:07:05 +0545

    single inheritance

---
+++ python/7A/new.py
@@ -0,0 +1,22 @@
+class person:
+    # this is the first class
+    def setname(self,name):
+        self.name=name
+    def getall1(self):
+        print(f"Name:{self.name}")
+
+
+class student(person):
+    # this is the child class
+    def setdetail(self,age,cl):
+        self.age=age
+        self.cl=cl
+    def getall(self):
+        print(f"Age:{self.age}\nClass:{self.cl}")
+object1=student()
+object1.setname("Ujwal")
+object1.setdetail(20,"BIM")
+object1.getall1();
+object1.getall()
+
+
