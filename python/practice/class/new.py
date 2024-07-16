5115ca9b504dc55cfff674c72b9ff9352a69baec
BMCBim <something123sdhgk@proton.me>
2024-07-16 14:40:10 +0545

    idk what i changed

--- python/practice/class/new.py
+++ python/practice/class/new.py
@@ -1,8 +1,12 @@
 class person:
  Name=""
  age=0
+ def setphno(self,ph):
+    self.phno=ph
 
 p1=person()
-print(p1.Name)
-p1.Name="Ujwal"
-print(p1.phn)
+p1.setphno(989898989898)
+print(p1.phno)
+# print(p1.Name)
+# p1.Name="Ujwal"
+# print(p1.phn)
