02580b3c4224c5f74555624646004ab0c41f06ad
nullproj <corremoa1@gmail.com>
2024-07-23 17:57:17 +0545

    renames to sensible names

---
+++ python/practice/oop/method overriding/override.py
@@ -0,0 +1,2 @@
+'''Write a base class Person with a method introduce() that prints "Hi, I'm a person." Create a subclass Student that overrides introduce() to print "Hi, I'm a student" but also calls the introduce() method from the Person class using super(). Instantiate the Student class and call introduce().
+'''
