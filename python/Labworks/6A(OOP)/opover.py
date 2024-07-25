522262bb70361c5c11bebfe2514a2d6aed0ccffb
BMCBim <something123sdhgk@proton.me>
2024-07-25 15:28:11 +0545

    gjfdjafasfa

--- python/Labworks/6A(OOP)/opover.py
+++ python/Labworks/6A(OOP)/opover.py
@@ -2,10 +2,13 @@ class distance:
     def __init__(self,km,m):
         self.km=km
         self.m=m
+
     def __lt__(one,two):
         return True if one.km<two.km else False
+
     def __mul__(one,two):
         return distance(one.km*two.km,one.m*two.m)
+
     def show(self):
         print(f"{self.km} kms and {self.m} meters")
 d1=distance(10,1)
