7868acf20f3221faa284a389aae13f24212b325d
nullproj <corremoa1@gmail.com>
2024-07-28 08:20:49 +0545

    function overriding

--- python/practice/oop/method overriding/override.py
+++ python/practice/oop/method overriding/override.py
@@ -1,2 +1,11 @@
 '''Write a base class Person with a method introduce() that prints "Hi, I'm a person." Create a subclass Student that overrides introduce() to print "Hi, I'm a student" but also calls the introduce() method from the Person class using super(). Instantiate the Student class and call introduce().
 '''
+class person:
+    def introduce(self):
+        print("Hi, I'm a person")
+class student(person):
+    def introduce(self):
+        print("Hi, I'm a student")
+        super().introduce()
+ob1=student()
+ob1.introduce()
