4127351c953cb45bbb23fa801bd6afb3e89fe4aa
nullproj <corremoa1@gmail.com>
2024-07-30 06:21:34 +0545

    added filepractice

--- python/practice/files/file.py
+++ python/practice/files/file.py
@@ -1,3 +1,14 @@
 fname="file.txt"
 this=open(fname,'r')
 print(this.read())
+import os
+this="/home/nullproj/Documents/BIM-5th-semester/python/practice/files/this.txt"
+fil=open(this,'r')
+print(fil.read())
+fil.close()
+fil=open(this,'a')
+fil.write("\nThis is the next line that i wrote using py")
+fil.close()
+fil=open(this,'r')
+print(fil.read())
+fil.close()
--- python/practice/oop/overloading/gt.py
+++ python/practice/oop/overloading/gt.py
@@ -1,6 +1,14 @@
 class currency:
-    def __init__(self,rupee):
+    def __init__(self,rupee,paisa):
         self.rupee=rupee
+        if paisa>100:
+            quo=paisa/100
+            self.rupee+quo
+            paisa=paisa%100
+            self.paisa=paisa
+        else:
+            self.paisa=paisa
+
     def __gt__(one,two):
         return True if one.rupee>two.rupee else False
 money=currency(100)
