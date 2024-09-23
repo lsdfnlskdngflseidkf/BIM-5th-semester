945f43bbb740265341bb54561865d0c1fa66643d
nullproj <corremoa1@gmail.com>
2024-09-23 08:53:12 +0545

    asdlkaskl

---
+++ python/practice/gui/test.py
@@ -0,0 +1,8 @@
+import tkinter
+m = tkinter.Tk()
+greeting = tkinter.Label(text="tkinter thingy in python")
+greeting.pack()
+'''
+widgets are added here
+'''
+m.mainloop()
---
+++ python/t.py
@@ -0,0 +1,47 @@
+import random
+from colorama import Fore, Back, Style, init
+
+# Initialize colorama
+init(autoreset=True)
+
+# Define the detailed Among Us character
+character = """
+⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀
+⠀⠀⣀⣴⣶⣶⣿⡟⠀⠀⠀⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀
+⠀⢰⣿⠋⠉⣹⣿⡇⠀⠀⠀⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀
+⠀⣸⣿⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀
+⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀
+⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀
+⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+
+# Define colors (excluding black as it's used for shading)
+colors = [Fore.RED, Fore.BLUE, Fore.GREEN, Fore.YELLOW, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
+
+def print_colored_character(color):
+    colored_char = ""
+    visor_chars = "⡿⢿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟"
+    for char in character:
+        if char in visor_chars or char in "⣤⣶⣿⣿⣿⠛⠛⠛⠛⠿⠟":
+            colored_char += f"{color}{char}{Style.RESET_ALL}"
+        else:
+            colored_char += char
+    print(colored_char)
+
+def main():
+    print("Welcome to the Detailed Among Us Character Printer!")
+    while True:
+        input("Press Enter to print a random colored Among Us character (or Ctrl+C to exit): ")
+        color = random.choice(colors)
+        print_colored_character(color)
+
+if __name__ == "__main__":
+    main()
