9d58d6e12844db9f058ee4e25d1c74e9ae89542f
BMCBim <something123sdhgk@proton.me>
2024-11-21 14:42:01 +0545

    Committing Changes

--- python/practice/gui/ooh.py
+++ python/practice/gui/ooh.py
@@ -1,7 +1,22 @@
 import tkinter as tk
-class App(tk.Tk):
-   def __init__(self):
-      super().__init__()
+from tkinter import *
+window = tk.Tk()
 
-app = App()
-app.mainloop()
+window.geometry("400x400")
+lb = tk.Label(text="This is a label in a window that uses tk interface")
+lb.pack()
+
+bt=tk.Button(text="Do NOT click this button")
+bt.pack()
+
+
+en=tk.Entry()
+enlab=tk.Label(text="Enter your name")
+enlab.pack()
+en.pack()
+name=en.get()
+en.delete(0,5)
+
+
+
+window.mainloop()
