856ae0f8be8db54c2ea23d33d8a11ced562261d7
nullproj <corremoa1@gmail.com>
2024-11-21 18:07:06 +0545

    committing all changes

--- python/practice/db/test.py
+++ python/practice/db/test.py
@@ -2,6 +2,7 @@ import mysql.connector
 #mydb = mysql.connector.connect(host = "localhost", user = "Nilesh", password = "nilesh123")
 mydb = mysql.connector.connect(user='root', password='')
 mycursor = mydb.cursor()
-mycursor.execute("show databases;")
+mycursor.execute("show
+    databases;")
 for i in mycursor:
     print (i)
